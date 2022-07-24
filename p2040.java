f272c717ed6bf39fb476b9660eb5d77263a8d5a2
xingchenchenya <93077588+xingchenchenya@users.noreply.github.com>
2022-07-24 23:44:29 +0800

    Add files via upload

---
+++ p2040.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class p2040 {
+    public static int n,count=0;
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static Set<String> set = new HashSet<>();
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+        Scanner sc =new Scanner(System.in);
+        n = sc.nextInt();
+        fun(0);
+        for (String value : set) {
+            System.out.println(value.replaceAll("", "+").substring(1));
+        }
+    }
+    public static void fun(int a){
+        if(a==n){
+            if(list.size()==1) return;
+            count++;
+            StringBuilder s = new StringBuilder();
+            for (Integer integer : list) {
+                s.append(integer);
+            }
+            set.add(s.toString());
+            return;
+        }
+        for (int i = 1; i <n+1; i++) {
+            if(a+i>n)
+                continue;
+            if(list.size()==0 || list.get(list.size()-1)<=i){
+                list.add(i);
+                a+=i;
+                fun(a);
+                list.remove(list.size()-1);
+                a-=i;
+            }
+        }
+    }
+}
