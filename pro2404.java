a4cca0a40279ac4843cf5656edd5877d423d591c
xingchenchenya <93077588+xingchenchenya@users.noreply.github.com>
2022-07-24 23:59:14 +0800

    Update pro2404.java

--- pro2404.java
+++ pro2404.java
@@ -2,17 +2,20 @@ import java.util.*;
 
 public class pro2404 {
     public static int n,count=0;
-    public static ArrayList<Integer> list = new ArrayList<>();
     public static Set<String> set = new HashSet<>();
+    public static ArrayList<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        StringBuilder s = new StringBuilder();
-        Scanner sc =new Scanner(System.in);
-        n = sc.nextInt();
+        //StringBuilder s = new StringBuilder();
+        Scanner scanner =new Scanner(System.in);
+        n = scanner.nextInt();
         fun(0);
         for (String value : set) {
             System.out.println(value.replaceAll("", "+").substring(1));
         }
     }
+    
+    
     public static void fun(int a){
         if(a==n){
             if(list.size()==1) return;
@@ -24,7 +27,7 @@ public class pro2404 {
             set.add(s.toString());
             return;
         }
-        for (int i = 1; i <n+1; i++) {
+        for (int i = 1; i<n+1; i++) {
             if(a+i>n)
                 continue;
             if(list.size()==0 || list.get(list.size()-1)<=i){
